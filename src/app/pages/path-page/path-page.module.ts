bd2b6494bd8c932f3c1f919d5b9ef9700c6fdb46
baneet-zs <baneet.dhawan@zopsmart.com>
2024-03-13 18:15:54 +0530

    fixed path-page.module.ts

--- src/app/pages/path-page/path-page.module.ts
+++ src/app/pages/path-page/path-page.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardsModule } from 'src/app/shared/cards/cards.module';
+import { PathPageContentComponent } from './path-page-wrapper/path-page-content/path-page-content.component';
+import { PathInfoComponent } from './path-info/path-info.component';
+import { PathPageWrapperComponent } from './path-page-wrapper/path-page-wrapper.component';
+import { PathPageComponent } from './path-page.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PathPageContentComponent,
+    PathInfoComponent,
+    PathPageComponent,
+    PathPageWrapperComponent,
+  ],
   imports: [CommonModule, CardsModule],
 })
 export class PathPageModule {}
