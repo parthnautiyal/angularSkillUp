409e767637b651e8f833a554cbc0856285ba24a2
baneet-zs <baneet.dhawan@zopsmart.com>
2024-03-13 18:12:02 +0530

    fixed course module

--- src/app/pages/course-page/course-page.module.ts
+++ src/app/pages/course-page/course-page.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { CoursesBannerComponent } from './courses-banner/courses-banner.component';
+import { CourseAboutSectionComponent } from './course-about-section/course-about-section.component';
+import { ChapterComponent } from './chapter/chapter.component';
+import { ChapterWrapperComponent } from './chapter/chapter-wrapper/chapter-wrapper.component';
+import { ChapterContentContainerComponent } from './chapter/chapter-wrapper/chapter-content-container/chapter-content-container.component';
+import { CoursePageComponent } from './course-page.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    CoursesBannerComponent,
+    CourseAboutSectionComponent,
+    ChapterComponent,
+    ChapterWrapperComponent,
+    ChapterContentContainerComponent,
+    CoursePageComponent,
+  ],
+  imports: [CommonModule],
 })
-export class CoursePageModule { }
+export class CoursePageModule {}
