c41e3bf3b0ac4c45858587348b1804e1e3c44b5a
chandan-zs <chandan.saha@zopsmart.com>
2024-03-13 21:09:56 +0530

    implemented dynamic cards for profile page without changing there import statements in other components

--- src/app/shared/cards/batch-card/batch-card.component.ts
+++ src/app/shared/cards/batch-card/batch-card.component.ts
@@ -3,14 +3,15 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-batch-card',
   templateUrl: './batch-card.component.html',
-  styleUrls: ['./batch-card.component.sass']
+  styleUrls: ['./batch-card.component.sass'],
 })
 export class BatchCardComponent implements OnInit {
+  isProfile: boolean =
+    localStorage.getItem('profile') === 'true' ? true : false;
 
-  @Input() batchData:any={};
-  
-  constructor() { }
+  @Input() batchData: any = {};
 
-  ngOnInit(): void {
-  }
+  constructor() {}
+
+  ngOnInit(): void {}
 }
