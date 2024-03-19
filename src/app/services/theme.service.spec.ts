62c87b3b62b31b919f3f19e317cb3ca4fa1870c2
prabal-ZS <prabal.sharma@zopsmart.com>
2024-03-19 16:20:13 +0530

    Added jasmine and karma dependencies and added testing for theme-service

--- src/app/services/theme.service.spec.ts
+++ src/app/services/theme.service.spec.ts
@@ -1,16 +1,44 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ThemeService } from './theme.service';
 
 describe('ThemeService', () => {
   let service: ThemeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [ThemeService]
+    });
     service = TestBed.inject(ThemeService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should initially be in light mode', () => {
+    expect(service.isDarkMode()).toBeTruthy();
+  });
+
+  it('should be able to set dark mode', () => {
+    service.setDarkMode(true);
+    expect(service.isDarkMode()).toBeTruthy();
+  });
+
+  it('should apply dark theme CSS class when dark mode is set', () => {
+    service.setDarkMode(true);
+    expect(document.body.classList.contains('dark-theme')).toBeTruthy();
+  });
+
+  it('should remove dark theme CSS class when dark mode is unset', () => {
+    service.setDarkMode(true);
+    service.setDarkMode(false);
+    expect(document.body.classList.contains('dark-theme')).toBeFalsy();
+  });
+
+  it('should apply light theme CSS class when dark mode is unset', () => {
+    service.setDarkMode(true);
+    service.setDarkMode(false);
+    expect(document.body.classList.contains('light-theme')).toBeTruthy();
+  });
 });
+
