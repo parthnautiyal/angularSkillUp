19c2977b812f3950934531376c970b549117adea
chandan-zs <chandan.saha@zopsmart.com>
2024-03-21 16:09:16 +0530

    corrected the review part in courses page

---
+++ src/app/models/Rating.ts
@@ -0,0 +1,8 @@
+export interface Rating {
+  [key: string]: number;
+  fiveStars: number;
+  fourStars: number;
+  threeStars: number;
+  twoStars: number;
+  oneStars: number;
+}
--- src/app/pages/course-page/course-reviews/course-reviews.component.ts
+++ src/app/pages/course-page/course-reviews/course-reviews.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Rating } from 'src/app/models/Rating';
 
 @Component({
   selector: 'app-course-reviews',
@@ -8,16 +9,64 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./course-reviews.component.sass'],
 })
 export class CourseReviewsComponent implements OnInit {
+  ratingClicked: number = 3;
+  isReviewOn: boolean = true;
+  isReviewCommentOn: boolean = false;
+  rating: Rating = {
+    fiveStars: 1,
+    fourStars: 2,
+    threeStars: 0,
+    twoStars: 0,
+    oneStars: 0,
+  };
+  ratingPercentage: number[] = [];
+
+  totalRating: number = 0;
+
   constructor(
     private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer,
+    private domSanitizer: DomSanitizer
   ) {
+    this.totalRating = Object.values(this.rating).reduce((a, b) => a + b, 0);
+    console.log('Rating Total -> ' + this.totalRating);
+    this.updateRating(this.rating);
+
     this.matIconRegistry.addSvgIcon(
       'star',
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '/assets/review-star.svg',
-      ),
+      this.domSanitizer.bypassSecurityTrustResourceUrl('/assets/star.svg')
     );
+    this.matIconRegistry.addSvgIcon(
+      'review-star',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        '/assets/review-star.svg'
+      )
+    );
+  }
+
+  handleReviewButton() {
+    this.isReviewOn = !this.isReviewOn;
+    this.isReviewCommentOn = false;
+  }
+  handleReviewCommentButton() {
+    this.isReviewOn = false;
+    this.isReviewCommentOn = !this.isReviewCommentOn;
+  }
+  handleMyReviewButton() {
+    this.isReviewCommentOn = false;
+  }
+
+  updateRating(rating: Rating) {
+    rating.fiveStars = (rating.fiveStars / this.totalRating) * 100;
+    rating.fourStars = (rating.fourStars / this.totalRating) * 100;
+    rating.threeStars = (rating.threeStars / this.totalRating) * 100;
+    rating.twoStars = (rating.twoStars / this.totalRating) * 100;
+    rating.oneStars = (rating.oneStars / this.totalRating) * 100;
+    this.ratingPercentage[0] = Math.floor(rating.fiveStars);
+    this.ratingPercentage[1] = Math.floor(rating.fourStars);
+    this.ratingPercentage[2] = Math.floor(rating.threeStars);
+    this.ratingPercentage[3] = Math.floor(rating.twoStars);
+    this.ratingPercentage[4] = Math.floor(rating.oneStars);
+    console.log(this.ratingPercentage);
   }
 
   ngOnInit(): void {}
