c4e6d5253771a48f00a2243c312ebe8a63f63a70
baneet-zs <baneet.dhawan@zopsmart.com>
2024-03-23 21:30:50 +0530

    added shimmer

--- src/app/constants/headingsTitle.ts
+++ src/app/constants/headingsTitle.ts
@@ -1,1 +1,2 @@
 export const HEADINGS_TITLE: string[] = ['batches', 'paths', 'courses'];
+export const ERROR: boolean[] = [false, false, false];
--- src/app/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,101 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { PathDataService } from 'src/app/services/path-data.service';
-import { Observable, combineLatest } from 'rxjs';
-import { Store } from '@ngrx/store';
-import { loadAllCourses } from 'src/app/state/action/course.action';
-import { loadAllBatches } from 'src/app/state/action/batch.action';
-import {
-  selectCourses,
-  selectCoursesError,
-} from 'src/app/state/selector/course.selector';
-import { Course } from 'src/app/models/Course';
-import { Batch } from 'src/app/models/Batch';
-import { selectBatchs } from 'src/app/state/selector/batch.selector';
-import { HEADINGS_TITLE } from 'src/app/constants/headingsTitle';
-import { loadAllPaths } from 'src/app/state/action/path.action';
-import { selectPaths } from 'src/app/state/selector/path.selector';
-import { Path } from 'src/app/models/Path';
 import { PrimeNGConfig } from 'primeng/api';
 import { MessageService } from 'primeng/api';
+import { ERROR, HEADINGS_TITLE } from 'src/app/constants/headingsTitle';
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.sass'],
 })
 export class DashboardPageComponent implements OnInit {
-  isError: boolean = false;
-  courses$!: Observable<Course[]>;
-  paths$!: Observable<Path[]>;
-  batch$!: Observable<Batch[]>;
-  error$!: Observable<any>;
   constructor(
-    private store$: Store,
     private primengConfig: PrimeNGConfig,
     private messageService: MessageService
   ) {
-    // if (localStorage.getItem('logginFlag') === 'true') {
     console.log('Logged In');
     this.showSuccess();
-    // localStorage.setItem('logginFlag', 'false');
-    // }
   }
   loading: boolean = true;
   headingsTitle = HEADINGS_TITLE;
+  error = ERROR;
 
-  ngOnInit(): void {
-    this.store$.dispatch(loadAllCourses());
-    this.store$.dispatch(loadAllBatches());
-    this.store$.dispatch(loadAllPaths());
-    this.courses$ = this.store$.select(selectCourses);
-    this.batch$ = this.store$.select(selectBatchs);
-    this.paths$ = this.store$.select(selectPaths);
-    this.error$ = this.store$.select(selectCoursesError);
-    // this.primengConfig.ripple = true;
-    this.courses$.subscribe((data) => {
-      if (localStorage.getItem('logginFlag') === 'true') {
-        console.log('Courses in dashboard -> ' + data);
-
-        this.showSuccess();
-      }
-    });
-
-    this.error$.subscribe((data) => {
-      console.log('Error -> ' + data);
-
-      if (data != null) {
-        this.isError = true;
-        this.showError(data);
-        // this.showSuccess();
-
-        this.loading = false;
-      } else {
-        this.showSuccess();
-      }
-    });
-
-    combineLatest([this.paths$, this.courses$, this.batch$]).subscribe(
-      ([pathsdata, batch, course]) => {
-        if (
-          typeof pathsdata === 'object' &&
-          Object.keys(pathsdata).length > 0 &&
-          typeof batch === 'object' &&
-          Object.keys(batch).length > 0 &&
-          typeof course === 'object' &&
-          Object.keys(course).length > 0
-        ) {
-          // Handle the combined results here
-          console.log('All data loaded');
-          console.log(pathsdata);
-          console.log(batch);
-          console.log(course);
-          localStorage.setItem('logginFlag', 'false');
-
-          this.loading = false;
-          // this.showError();
-        }
-      }
-    );
-  }
+  ngOnInit(): void {}
   showError(data: string) {
     this.messageService.add({
       severity: 'error',
