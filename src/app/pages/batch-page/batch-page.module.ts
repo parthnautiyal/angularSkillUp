e4dc7b579c081261d4e4fe99b38abdfad098d448
baneet-zs <baneet.dhawan@zopsmart.com>
2024-03-24 21:52:57 +0530

    added batch page loading

--- src/app/pages/batch-page/batch-page.module.ts
+++ src/app/pages/batch-page/batch-page.module.ts
@@ -6,9 +6,11 @@ import { BatchDetailsComponent } from './batch-details/batch-details.component';
 import { PathPageModule } from '../path-page/path-page.module';
 import { ContainersModule } from 'src/app/shared/containers/containers.module';
 import { BatchPageRoutingModule } from './batch-page-routing.module';
+import { ShimmerComponent } from 'src/app/shimmer/shimmer.component';
+import { ShimmerModule } from 'src/app/shimmer/shimmer.module';
 
 @NgModule({
   declarations: [BatchPageComponent, BatchDetailsComponent],
-  imports: [CommonModule, CardsModule, ContainersModule,BatchPageRoutingModule],
+  imports: [CommonModule, CardsModule, ContainersModule,BatchPageRoutingModule,ShimmerModule],
 })
 export class BatchPageModule {}
