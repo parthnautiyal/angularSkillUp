9ffd4ca9fbb231baa946ff1f5ec6bfde403845e2
ParthNautiyal-ZS <parth.nautiyal@zopsmart.com>
2024-03-25 06:00:53 +0530

    made dashboard header course dynamicand added link

--- src/app/pages/dashboard-page/dashboard-header/dashboard-header.component.ts
+++ src/app/pages/dashboard-page/dashboard-header/dashboard-header.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
@@ -5,6 +6,8 @@ import { loadNoOfEnrolledCourses } from 'src/app/state/action/course.action';
 import { loadNumberOfEnrolledPaths } from 'src/app/state/action/path.action';
 import { selectNoOfCourses } from 'src/app/state/selector/course.selector';
 import { selectNoOfEnrolledPaths } from 'src/app/state/selector/path.selector';
+import { enrolledCourses } from '../../../models/EnrolledCourses';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-header',
@@ -13,10 +16,15 @@ import { selectNoOfEnrolledPaths } from 'src/app/state/selector/path.selector';
 })
 export class DashboardHeaderComponent implements OnInit {
   userProfile: any;
+  enrolledCourse: any;
   enrolledPathsNumber: number = 0;
   enrolledCoursesNumber: number = 0;
   noOfEnrolledCourses!: Observable<Number>;
-  constructor(private store$: Store) {}
+  constructor(
+    private store$: Store,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.store$.dispatch(loadNoOfEnrolledCourses());
@@ -29,5 +37,19 @@ export class DashboardHeaderComponent implements OnInit {
     });
 
     this.userProfile = JSON.parse(sessionStorage.getItem('loggedInUser') || '');
+
+    this.http
+      .get('https://api.training.zopsmart.com/students/enrolled-courses')
+      .subscribe((response) => {
+        const data = response as any;
+        const courses = data.data.enrolledCourses;
+        if (courses.length > 0) {
+          const randomIndex = Math.floor(Math.random() * courses.length);
+          this.enrolledCourse = courses[randomIndex];
+        }
+      });
+  }
+  navigateToCourse() {
+    this.router.navigate(['/course', this.enrolledCourse.courseId]);
   }
 }
