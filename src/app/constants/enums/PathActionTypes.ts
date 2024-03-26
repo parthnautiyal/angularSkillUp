a4da15809053da5e382fd90f8ae9079a53b8545f
ParthNautiyal-ZS <parth.nautiyal@zopsmart.com>
2024-03-26 14:13:19 +0530

    store enums

---
+++ src/app/constants/enums/PathActionTypes.ts
@@ -0,0 +1,6 @@
+export enum PathActionTypes {
+  LOAD_ALL_PATHS = '[Path] Load All Paths',
+  LOAD_ALL_PATHS_SUCCESS = '[Path] Load All Paths Success',
+  LOAD_ALL_PATHS_FAILED = '[Path] Load All Paths Failed',
+  LOAD_PATH_BY_ID = '[Path] Load Path By ID',
+}
