0f94e50d7c76e9a3e4bfca9582a8de38f4719449
ParthNautiyal-ZS <parth.nautiyal@zopsmart.com>
2024-03-27 15:08:21 +0530

    resolved merge conflicts

--- src/app/state/effects/batch.effects.ts
+++ src/app/state/effects/batch.effects.ts
@@ -42,7 +42,7 @@ export class BatchEffects {
           .get<APIResponse<EnrolledBatches>>(
             API.BASE_URL +
               API.STUDENT +
-              '/330' +
+              '/329' +
               API.ENROLLED_BATCHES +
               API.PAGE_SIZE
           )
